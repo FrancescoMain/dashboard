5a1288a074ffcf3bdd35903c960b515f1132c10b
Carlo <carlorebuffat412@gmail.com>
2023-04-05 17:55:46 +0200

    add: modal

--- src/components/Projects/ProjectsList.tsx
+++ src/components/Projects/ProjectsList.tsx
@@ -4,52 +4,65 @@ import DeleteIcon from '@mui/icons-material/Delete';
 import EditIcon from '@mui/icons-material/Edit';
 import { deleteProject } from "../../redux/projects/projectSlice";
 import { useNavigate } from "react-router-dom";
+import { Modal, ModalBody, ModalBox } from "./ProjectsListStyle";
 
 const Projects = () => {
   const dispatch = useDispatch();
   const projects = useAppSelector((state) => state.projects);
+  // Quando si clicca sull'icona delete, si setta lo stato openModal a true -> si dà la classe show-modal al Modal
   const navigate = useNavigate();
-  console.log(projects);
-
   const handleDeleteProject = (projectId: number) => {
     dispatch(deleteProject(projectId));
   }
   
   return (
-    <div className="table-wrapper">
-      <table className="fl-table">
-        <thead>
-          <tr>
-              <th>Title</th>
-              <th>Deadline</th>
-              <th>Company</th>
-              <th>Assigned to</th>
-              <th>Actions</th>
-          </tr>
-        </thead>
-        <tbody>
-            {projects.map((project) => {
-              return (
-                <>
-                  <tr>
-                    <td>{project.title}</td>
-                    <td>{project.deadline.toLocaleDateString()}</td>
-                    <td>{project.company}</td>
-                    <td>{project.assigned_to}</td>
-                    <td>
-                      <DeleteIcon style={{cursor: 'pointer'}} onClick={() => handleDeleteProject(project.id)}/>
-                      
-                      <a onClick={() => navigate(`/projects/edit/${project.id}`)}>
-                      <EditIcon style={{cursor: 'pointer'}}/>
-                      </a>
-                    </td>
-                  </tr>
-                </>
-              )
-            })}
-        </tbody>
-      </table>
-    </div>
+    <>
+      <Modal>
+        <ModalBox>
+          Confirmation
+        </ModalBox>
+        <ModalBody>
+          Eliminare davvero questo progetto?
+        </ModalBody>
+        <ModalBox>
+          <button className="me-1">Sì</button>
+          <button>Annulla</button>
+        </ModalBox>
+      </Modal>
+      <div className="table-wrapper">
+        <table className="fl-table">
+          <thead>
+            <tr>
+                <th>Title</th>
+                <th>Deadline</th>
+                <th>Company</th>
+                <th>Assigned to</th>
+                <th>Actions</th>
+            </tr>
+          </thead>
+          <tbody>
+              {projects.map((project) => {
+                return (
+                  <>
+                    <tr>
+                      <td>{project.title}</td>
+                      <td>{project.deadline.toLocaleDateString()}</td>
+                      <td>{project.company}</td>
+                      <td>{project.assigned_to}</td>
+                      <td>
+                        <a onClick={() => navigate(`/projects/edit/${project.id}`)}>
+                        <EditIcon style={{cursor: 'pointer'}}/>
+                        </a>
+                        <DeleteIcon style={{cursor: 'pointer'}} onClick={() => handleDeleteProject(project.id)}/>
+                      </td>
+                    </tr>
+                  </>
+                )
+              })}
+          </tbody>
+        </table>
+      </div>
+    </>
   );
 };
 
---
+++ src/components/Projects/ProjectsListStyle.ts
@@ -0,0 +1,23 @@
+import styled from "styled-components";
+
+export const Modal = styled.div`
+background-color: white;
+width: 500px;
+min-height: 100px;
+position: absolute;
+top: 20%;
+left: 50%;
+transform: translate(-50%, -50%);
+`
+
+export const ModalBody = styled.div`
+padding: 1rem;
+font-size: .9rem;
+border-top: 1px solid lightgrey;
+border-bottom: 1px solid lightgrey;
+`
+
+export const ModalBox = styled.div`
+padding: 1rem;
+font-size: .9rem;
+`
